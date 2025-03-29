98165cbd52a3aae0828794b4b96ddbba607f2627
Yuhei Shibata <shibattyo.obama@gmail.com>
2025-03-29 21:18:16 +0900

    feat:第九章の実装とテスト

--- src/main/java/com/example/tdd/Dollar.java
+++ src/main/java/com/example/tdd/Dollar.java
@@ -1,14 +1,10 @@
 package com.example.tdd;
 
-/**
- * Hello world!
- *
- */
 public class Dollar extends Money {
-    Dollar(int amount) {
-        this.amount = amount;
+    Dollar(int amount, String currency) {
+        super(amount, currency);
     }
     Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
 }
